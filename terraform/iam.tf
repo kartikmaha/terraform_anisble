586e2f4d3675ea0056d7806822276da3a51fdea8
kartikmaha <kartikmahashabde@gmail.com>
2026-04-10 06:32:52 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,9 +1,9 @@
 # ========================================
-# IAM Role for EC2 (Optional but Recommended)
+# IAM Role for EC2
 # ========================================
 
 resource "aws_iam_role" "ec2_role" {
-  name = "${var.project_name}-${var.env}-ec2-role"
+  name = "${var.project_name}-${var.environment}-ec2-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -15,16 +15,30 @@ resource "aws_iam_role" "ec2_role" {
       Action = "sts:AssumeRole"
     }]
   })
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-ec2-role"
+  }
 }
 
-# Attach SSM policy (optional but powerful)
+# ========================================
+# Attach SSM Policy (for SSH-less access)
+# ========================================
+
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# ========================================
 # Instance Profile
+# ========================================
+
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "${var.project_name}-${var.env}-profile"
+  name = "${var.project_name}-${var.environment}-profile"
   role = aws_iam_role.ec2_role.name
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-profile"
+  }
 }
